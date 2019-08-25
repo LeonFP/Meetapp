b75f2d29773046100d29f4506fbd65bf4ae7f45e
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-08-25 16:32:23 -0300

    Configuração do sequelize, criação de model de Users e loader das Models

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+// https://sequelize.org/master/
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
